18aebd022999c3370bbaae505c83ca29dd5735d1
Heber Uriel <hurielarrascaeta@gmail.com>
2024-12-29 19:37:04 -0300

    tiendapy03 navbar & section libros

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def home():
     return render_template('sitio/index.html')
 
 
+@app.route('/libros')
+def libros():
+    return render_template('sitio/libros.html')
+
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('sitio/nosotros.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
